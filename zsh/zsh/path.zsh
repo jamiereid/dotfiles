cad20c8f82eb1f40814be3beb5d96b089cd6ea1c
Jamie Reid <jamie@jre.id.au>
2015-11-24 17:21:24 +1100

    Changes to path

--- zsh/zsh/path.zsh
+++ zsh/zsh/path.zsh
@@ -3,15 +3,16 @@ typeset -U path
 # prepends
 #path=(~/.rbenv/bin "$path[@]")
 
-# appends
-path+=(~/bin)
-path+=($DOTSPATH/bin)
-
-# prune paths that don't exist
-path=($^path(N))
-
 if [[ $OSTYPE == 'cygwin' ]]; then
 	export GOPATH="/cygdrive/c/Go/"
 else
-	export GOPATH="~/Code/go"
+	export GOPATH="$HOME/Code/go"
 fi
+
+# appends
+path+=(~/bin)
+path+=($DOTSPATH/bin)
+path+=($GOPATH/bin)
+
+# prune paths that don't exist
+path=($^path(N))
