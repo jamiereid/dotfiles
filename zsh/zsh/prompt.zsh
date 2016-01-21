8c6ee688cdf80862306445879b64a943b83922cd
Jamie Reid <jamie@jre.id.au>
2016-01-21 15:05:57 +1100

    Change prompt

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -1,16 +1,6 @@
 # prompt
 setopt prompt_subst
 
-# mode-aware arrow
-
-function p_arrow {
-  if [[ $KEYMAP = "vicmd" ]]; then
-    echo "%F{magenta}»%f"
-  else
-    echo "%F{cyan}»%f"
-  fi
-}
-
 # colored path
 
 function p_colored_path {
@@ -33,13 +23,15 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 function p_envs {
   local envs
-  [[ -n $SSH_CLIENT ]]  && envs+="R"
   [[ -n $VIRTUAL_ENV ]] && envs+="P"
-
   [[ -n $envs ]] && echo " %F{green}[%f$envs%F{green}]%f"
 }
 
+function p_remote {
+  [[ -n $SSH_CLIENT ]] && echo "%F{grey}@%m%f"
+}
+
 PROMPT='
-%F{blue}λ%f $(p_colored_path)$(p_envs)$(p_vcs)
-$(p_arrow) '
+%F{red}λ%f %F{green}%n%f$(p_remote):$(p_colored_path)$(p_envs)$(p_vcs)
+%F{cyan}»%f '
 
