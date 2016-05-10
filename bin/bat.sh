e6670bcd22f14ab2e3d91e9d9e0e4e949ec59237
Jamie Reid <jamie@jre.id.au>
2016-05-10 11:54:15 +1000

    Changes to i3 config
    - Change theme to gotham
    - Change some workspaces around
    - Remove some lines we no longer need
    - Add some startup applications
    - Change i3blocks config
    - Add some additional scripts for i3blocks
    - Make some changes to exisiting scripts for i3blocks

--- bin/bat.sh
+++ bin/bat.sh
@@ -1,6 +1,46 @@
-MAX0="$(cat /sys/class/power_supply/BAT0/charge_full)"
-CAP0="$(cat /sys/class/power_supply/BAT0/charge_now)"
+COLGOOD="#2E9D54"
+COLBAD="#cc6666"
+COLDEGRADED="#81a2be"
 
-BAT="$(echo "100*(${CAP0})/(${MAX0})" | bc)"
+BAT=$(upower -e | grep BAT)
+STATE=$(upower -i $BAT | grep state | awk '{print $2}')
+CURRENT_PERCENT=$(upower -i $BAT | grep percentage | awk '{print $2}')
 
-echo $BAT%
+case "$CURRENT_PERCENT" in
+    [90-100]*)
+        BATICON="  "
+        RETCOLOR=$COLGOOD
+        ;;
+    [89-51]*)
+        BATICON="  "
+        RETCOLOR=$COLGOOD
+        ;;
+    [50-31]*)
+        BATICON="  "
+        RETCOLOR=$COLDEGRADED
+        ;;
+    [30-11]*)
+        BATICON="  "
+        RETCOLOR=$COLDEGRADED
+        ;;
+    *)
+        BATICON="  "
+        RETCOLOR=$COLBAD
+        ;;
+esac
+
+if [[ "$STATE" = "discharging" ]]; then
+    REMAINING=$(upower -i $BAT | grep "time to empty" | awk '{print $4" "$5}')
+    RET="$BATICON $CURRENT_PERCENT ($REMAINING)"
+elif [[ "$STATE" = "charging" ]]; then
+    REMAINING=$(upower -i $BAT | grep "time to full" | awk '{print $4" "$5}')
+    RET=" $CURRENT_PERCENT ($REMAINING)"
+    RETCOLOR=$COLGOOD
+else
+    RET=" $CURRENT_PERCENT"
+    RETCOLOR=$COLGOOD
+fi
+
+echo $RET
+echo
+echo $RETCOLOR
--- bin/wifi.sh
+++ bin/wifi.sh
@@ -1,14 +1,26 @@
+COLGOOD="#2E9D54"
+COLBAD="#cc6666"
+COLDEGRADED="#81a2be"
+
 IWPRESENT="$(ifconfig | grep wlp)"
 
 if [[ -n "$IWPRESENT" ]]; then
+    INT="$(ifconfig | grep wlp | awk -F: '{print $1}')"
     SSID="$(iwgetid -r)"
     if [[ -z "$SSID" ]]; then
-        RET=""
+        RET=" down"
+		RETCOLOR=$COLBAD
     else
-        RET=" $SSID"
+        SIGNAL="$(iwconfig $INT | sed -n 's/.*Link Quality=\([0-9][0-9]*\/[0-9][0-9]*\).*/\1/p')"
+        IP="$(ip addr show $INT | grep "inet " | awk '{print $2}')"
+        RET=" $SIGNAL at $SSID ($IP)"
+		RETCOLOR=$COLGOOD
     fi 
 else
-    RET=""
+    RET=" Disabled"
+    RETCOLOR=$COLBAD
 fi
 
 echo $RET
+echo
+echo $RETCOLOR
