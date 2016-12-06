e341e1f5de47495085022dac3c331f5ad5b9d842
Jamie Reid <jamie@jre.id.au>
2016-12-06 11:42:55 +1100

    Avoid variables for now in orgsettings

--- emacs/.emacs.d/orgsettings.el
+++ emacs/.emacs.d/orgsettings.el
@@ -5,12 +5,11 @@
 (setq debug-on-error t)
 
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
-(require 'org-install)
 (require 'org)
 
-(setq org-dir "~/org/")
+;;(setq org-dir "~/org/")
 ;;(setq org-directory org-dir)
-(setq org-agenda-files (quote (org-dir)))
+(setq org-agenda-files (quote ("~/org")))
 
 ;; Standard key bindings
 (global-set-key "\C-cl" 'org-store-link)
@@ -40,17 +39,17 @@
               ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
 
 
-(setq org-default-notes-file (concat org-dir "refile.org"))
+(setq org-default-notes-file "~/org/refile.org")
 
-;;(setq org-capture-templates
-      ;; (quote (("t" "todo" entry (file (concat org-dir "refile.org"))
-      ;;          "* TODO %?\n%U\n%a\n")
-      ;;         ("n" "note" entry (file (concat org-dir "refile.org"))
-      ;;          "* %? :NOTE:\n%U\n%a\n")
-      ;;         ("j" "journal" entry (file+datetree (concat org-dir "journal.org"))
-      ;;          "* %?\n%U\n")
-      ;;         ("m" "meeting" entry (file (concat org-dir "refile.org"))
-      ;;          "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-      ;;         ("b" "media" checkitem (file (concat org-dir "refile.org"))
-      ;;          "[ ] %?"))))
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/org/refile.org")
+               "* TODO %?\n%U\n%a\n")
+              ("n" "note" entry (file "~/org/refile.org")
+               "* %? :NOTE:\n%U\n%a\n")
+              ("j" "journal" entry (file+datetree "~/org/journal.org")
+               "* %?\n%U\n")
+              ("m" "meeting" entry (file "~/org/refile.org")
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("b" "media" checkitem (file "~/org/refile.org")
+               "[ ] %?"))))
 
