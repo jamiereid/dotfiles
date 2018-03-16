a8a3c83a796c95a1b8bf9ea5c9efb894c8d23a5e
Jamie Reid <jamie@jre.id.au>
2018-03-16 16:40:55 +1100

    babel lang updates

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -29,31 +29,3 @@
 (org-babel-load-file
   (expand-file-name "settings.org"
                     user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
- '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(package-selected-packages
-   (quote
-    (org-pdfview web-mode use-package rainbow-mode pdf-tools org-bullets json-mode flycheck expand-region diminish counsel company color-theme-zenburn)))
- '(pdf-tools-handle-upgrades nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "blue4" :foreground "gray90"))))
- '(mode-line-buffer-id ((t (:foreground "#f0dfaf" :weight ultra-bold))))
- '(mode-line-inactive ((t (:background "#404045" :foreground "gray60"))))
- '(org-block ((t (:background "#333333"))))
- '(org-block-background ((t (:background "#333333"))))
- '(org-block-begin-line ((t (:foreground "#6f6f6f" :background "#1e2320"))) t)
- '(org-block-end-line ((t (:foreground "#6f6f6f" :background "#1e2320"))) t)
- '(show-paren-match ((t (:background "default" :foreground "#ecbcbc" :weight ultra-bold))))
- '(show-paren-mismatch ((t (:background "default" :foreground "#9fafaf" :weight ultra-bold))))
- '(which-func ((t (:foreground "orange")))))
