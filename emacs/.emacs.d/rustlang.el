9891f0899fcb76014e38565d108638a19ca6bd97
Jamie Reid <jamie@jre.id.au>
2018-12-26 20:35:57 +1100

    Usual 'new install' changes

--- emacs/.emacs.d/rustlang.el
+++ emacs/.emacs.d/rustlang.el
@@ -20,8 +20,8 @@
 (use-package racer
   :ensure t
   :config (progn
-            (setq racer-cmd "c:/Support/rust/cargohome/bin/racer")
-            (setq racer-rust-src-path "c:/Support/rust/rustuphome/toolchains/nightly-x86_64-pc-windows-msvc/lib/rustlib/src/rust/src"))
+            (setq racer-cmd "~/.cargo/bin/racer")
+            (setq racer-rust-src-path "~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"))
   :init (progn
           (add-hook 'rust-mode-hook #'racer-mode)
           (add-hook 'racer-mode-hook #'eldoc-mode)
