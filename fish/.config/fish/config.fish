b1ef038d2cccc78acd7278ab245c42634112cd1d
Jamie Reid <jamie@jre.id.au>
2019-02-23 19:45:45 +1100

    update prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -78,22 +78,44 @@ function sudo-commandline -d "Insert sudo at the beginning of the command line"
     commandline -r -- "sudo $cmdline"
 end
 
+#function fish_prompt
+#	set_color brblack
+#	echo -n "["(date "+%H:%M")"] "
+#	set_color blue
+#	echo -n (hostname)
+#	if [ $PWD != $HOME ]
+#		set_color brblack
+#		echo -n ':'
+#		set_color yellow
+#		echo -n (basename $PWD)
+#	end
+#	set_color green
+#	printf '%s ' (__fish_git_prompt)
+#	set_color red
+#	echo -n '| '
+#	set_color normal
+#end
+
 function fish_prompt
-	set_color brblack
-	echo -n "["(date "+%H:%M")"] "
-	set_color blue
-	echo -n (hostname)
-	if [ $PWD != $HOME ]
-		set_color brblack
-		echo -n ':'
-		set_color yellow
-		echo -n (basename $PWD)
-	end
-	set_color green
-	printf '%s ' (__fish_git_prompt)
-	set_color red
-	echo -n '| '
-	set_color normal
+  set_color white
+  echo -n "["(date "+%H:%M")"] "
+  set_color yellow
+  echo -n (whoami)
+  set_color normal
+  echo -n '@'
+  set_color blue
+  echo -n (hostname)" "
+  set_color green
+  echo -n (prompt_pwd)
+  set_color brown
+  printf '%s ' (__fish_git_prompt)
+  set_color red
+  if [ (whoami) = "root" ]
+    echo -n '# '
+  else
+    echo -n '$ '
+  end
+  set_color normal
 end
 
 function fish_greeting
