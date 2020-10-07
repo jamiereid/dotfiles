1177a08eb66a9d8ca65c2fc5ffbf103e5f25e398
Jamie Reid <jamie@jre.id.au>
2020-10-07 17:41:37 +1100

    fish: make wsl stuff a little nicer

--- fish/.config/fish/368238L.fish
+++ fish/.config/fish/368238L.fish
@@ -1,8 +1,9 @@
 #set -gx DOCKER_HOST tcp://localhost:2375
+set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+set -gx LIBGL_ALWAYS_INDIRECT 1
 
 # hack for wsl?
 cd $HOME
 
 abbr -a doxfx mono /opt/docfx-git/docfx.exe
-#abbr -a e env DISPLAY=(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0 setsid emacs
-abbr -a t env DISPLAY=(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0 LIBGL_ALWAYS_INDIRECT=1 setsid xfce4-terminal
+abbr -a t setsid xfce4-terminal
