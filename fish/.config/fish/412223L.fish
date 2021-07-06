3f0fbeee6dde946cce1d2151658b46d55272c163
Jamie Reid <jamie@jre.id.au>
2021-07-06 17:20:30 +1000

    fish: work laptop updates

--- fish/.config/fish/412223L.fish
+++ fish/.config/fish/412223L.fish
@@ -1,10 +1,10 @@
 #set -gx DOCKER_HOST tcp://localhost:2375
-#set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
-#set -gx LIBGL_ALWAYS_INDIRECT 1
+set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+set -gx LIBGL_ALWAYS_INDIRECT 1
 
 # hack for wsl?
 cd $HOME
 doas ~/.local/bin/dnsfix
 
 #abbr -a doxfx mono /opt/docfx-git/docfx.exe
-#abbr -a t setsid xfce4-terminal
+abbr -a t setsid xfce4-terminal
