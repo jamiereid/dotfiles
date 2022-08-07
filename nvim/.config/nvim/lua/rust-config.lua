45ff88e2458e63e008b3532da7fc1adaccbc729e
Jamie Reid <jamie@jre.id.au>
2022-08-07 19:55:56 +1000

    nvim: addional key bindings for rust lsp

--- nvim/.config/nvim/lua/rust-config.lua
+++ nvim/.config/nvim/lua/rust-config.lua
@@ -1,3 +1,30 @@
+local map = function(type, key, value)
+	vim.api.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true});
+end
+
+local custom_attach = function(client)
+	print("LSP started.");
+	--require'completion'.on_attach(client)
+	--require'diagnostic'.on_attach(client)
+
+	map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+	map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+	map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
+	map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+	map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+	map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+	map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+	--map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+	--map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+	--map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+	--map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+	--map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+	--map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
+	--map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+	--map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+	--map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+end
+
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
@@ -63,5 +90,6 @@ cmp.setup.cmdline(':', {
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 require('lspconfig')['rust_analyzer'].setup {
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = custom_attach
 }
