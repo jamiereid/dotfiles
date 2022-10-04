3b15748ca03dd2423df72d244a1a41f18580b641
Jamie Reid <jamie@jre.id.au>
2022-10-04 18:33:02 +1100

    nvim: more telescope keybindings

--- nvim/.config/nvim/lua/packages-config.lua
+++ nvim/.config/nvim/lua/packages-config.lua
@@ -86,3 +86,9 @@ vim.api.nvim_set_keymap(
   ":Telescope file_browser<enter>",
   { noremap = true }
 )
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+vim.keymap.set('n', 'fh', builtin.help_tags, {})
