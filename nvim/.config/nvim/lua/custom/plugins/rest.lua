8375a1eee2a2865fccea56fffbc1c846bc540e6a
Jamie Reid <jamie@jre.id.au>
2025-08-21 20:37:48 +1000

    don't conceal in kulala buffers

--- nvim/.config/nvim/lua/custom/plugins/rest.lua
+++ nvim/.config/nvim/lua/custom/plugins/rest.lua
@@ -30,5 +30,22 @@ return {
       global_keymaps_prefix = "<leader>r",
       kulala_keymaps_prefix = "",
     },
+    config = function(_, opts)
+      require("kulala").setup(opts)
+
+      local aug = vim.api.nvim_create_augroup("KulalaUiFixes", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufWinEnter", "FileType" }, {
+        group = aug,
+        callback = function(ev)
+          local name = vim.api.nvim_buf_get_name(ev.buf)
+          local ft = vim.bo[ev.buf].filetype or ""
+          if (name and name:match "^kulala://ui") or ft:find("kulala_ui", 1, true) then
+            -- Stop quotes being concealed in the UI pane
+            vim.opt_local.conceallevel = 0
+            vim.opt_local.concealcursor = ""
+          end
+        end,
+      })
+    end,
   },
 }
