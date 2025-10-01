11649068b80802fce5eba98e3635dd3ed7e31141
Jamie Reid <jamie@jre.id.au>
2025-10-01 17:02:11 +1000

    improvements to codecompanion config

--- nvim/.config/nvim/lua/custom/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/custom/plugins/codecompanion.lua
@@ -4,41 +4,16 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "j-hui/fidget.nvim",
+      "franco-ruggeri/codecompanion-spinner.nvim",
     },
     keys = {
       { "<C-a>", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, noremap = true, silent = true },
       { "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, noremap = true, silent = true },
     },
-    init = function()
-      local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", {})
-      local fidget_handle = nil
-
-      vim.api.nvim_create_autocmd({ "User" }, {
-        pattern = "CodeCompanionRequestStarted",
-        group = group,
-        callback = function(request)
-          local fidget_progress = require "fidget.progress"
-          fidget_handle = fidget_progress.handle.create {
-            title = "CodeCompanion",
-            message = "Requesting assistance...",
-            lsp_client = { name = "CodeCompanion" },
-          }
-        end,
-      })
-
-      vim.api.nvim_create_autocmd({ "User" }, {
-        pattern = "CodeCompanionRequestFinished",
-        group = group,
-        callback = function(request)
-          if fidget_handle then
-            fidget_handle.message = "Done"
-            fidget_handle:finish()
-            fidget_handle = nil
-          end
-        end,
-      })
-    end,
     opts = {
+      extensions = {
+        spinner = {},
+      },
       adapters = {
         acp = {
           claude_code = function()
@@ -69,10 +44,35 @@ return {
       },
       strategies = {
         chat = { adapter = "claude_code" },
-        inline = { adapter = "openai" },
+        inline = {
+          adapter = "openai",
+          keymaps = {
+            accept_change = {
+              modes = { n = "gda" },
+            },
+            reject_change = {
+              modes = { n = "gdr" },
+            },
+            always_accept = {
+              modes = { n = "gdy" },
+            },
+          },
+        },
       },
       display = {
         chat = {
+          intro_message = "walrus...",
+          diff_window = {
+            width = function()
+              return math.min(120, vim.o.columns - 10)
+            end,
+            height = function()
+              return vim.o.lines - 4
+            end,
+            opts = {
+              number = true,
+            },
+          },
           window = {
             opts = {
               spell = true,
